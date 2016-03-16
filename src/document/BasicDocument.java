8fd2a09e3302effc22e4351946fc4486d69bd12c
eliyooy <ebshimon@gmail.com>
2016-03-16 19:41:45 -0400

    Added countSyllables, getNumWords, getNumSentences, getNumSyllables, and additional tests

--- src/document/BasicDocument.java
+++ src/document/BasicDocument.java
@@ -1,9 +1,11 @@
 package document;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /** 
- * A naive implementation of the Document abstract class. 
+ * A native implementation of the Document abstract class.
  * @author UC San Diego Intermediate Programming MOOC team
  */
 public class BasicDocument extends Document 
@@ -30,7 +32,10 @@ public class BasicDocument extends Document
 	{
 		//TODO: Implement this method.  See the Module 1 support videos 
 	    // if you need help.
-	    return 0;
+
+		List<String> tokens = getTokens( "[a-zA-Z]+" );
+
+		return tokens.size();
 	}
 	
 	/**
@@ -46,7 +51,15 @@ public class BasicDocument extends Document
 	{
 	    //TODO: Implement this method.  See the Module 1 support videos 
         // if you need help.
-        return 0;
+
+		String text = this.getText();
+
+		String[] result = text.split("[.!?]+");
+
+		if(text != "") {
+			return result.length;
+		}
+        	return 0;
 	}
 	
 	/**
@@ -62,7 +75,16 @@ public class BasicDocument extends Document
 	{
 	    //TODO: Implement this method.  See the Module 1 support videos 
         // if you need help.
-        return 0;
+
+		String text = this.getText();
+		String[] result = text.split("[ ,.?!0-9():]+");
+		int syllCount = 0;
+
+		for(String resulting : result) {
+			syllCount += countSyllables(resulting);
+		}
+
+		return syllCount;
 	}
 	
 	
@@ -70,22 +92,66 @@ public class BasicDocument extends Document
 	 * You are encouraged to add your own tests.  */
 	public static void main(String[] args)
 	{
+		// *** First test sentence ***
 		testCase(new BasicDocument("This is a test.  How many???  "
 		        + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
 				16, 13, 5);
+		BasicDocument test1 = new BasicDocument("This is a test.  How many???  "
+				+ "Senteeeeeeeeeences are here... there should be 5!  Right?");
+
+		// *** Testing the first sentence ***
+		String text1 = test1.getText();
+		String[] text1a = text1.split("[ ,.?!0-9():]+");
+		System.out.println(Arrays.toString(text1a));
+
+		// *** Testing individual words ***
+		String hello = "segue";
+		String[] wordSplit = hello.split("[^aeiouy]+");
+		System.out.println(Arrays.toString(wordSplit));
+
+		BasicDocument testDoc = new BasicDocument("");
+		int result = testDoc.countSyllables("segue");
+		if(hello.endsWith("e") && hello.charAt(hello.length() - 2) != 'u') {
+			System.out.println("You're in the block");
+		} else {
+			System.out.println("You skipped the block");
+		}
+		System.out.println(result);
+
+
+		// *** Remaining Tests ***
 		testCase(new BasicDocument(""), 0, 0, 0);
+		BasicDocument test2 = new BasicDocument("");
+
 		testCase(new BasicDocument("sentence, with, lots, of, commas.!  "
 		        + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
+		BasicDocument test3 = new BasicDocument("sentence, with, lots, of, commas.!  "
+				+ "(And some poaren)).  The output is: 7.5.");
+
 		testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
+		BasicDocument test4 = new BasicDocument("many???  Senteeeeeeeeeences are");
+
 		testCase(new BasicDocument("Here is a series of test sentences. Your program should "
 				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
 				+ "the correct amount of syllables (example, for example), "
 				+ "but most of them will."), 49, 33, 3);
+		BasicDocument test5 = new BasicDocument("Here is a series of test sentences. Your program should "
+				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
+				+ "the correct amount of syllables (example, for example), "
+				+ "but most of them will.");
+
 		testCase(new BasicDocument("Segue"), 2, 1, 1);
+		BasicDocument test6 = new BasicDocument("Segue");
+
 		testCase(new BasicDocument("Sentence"), 2, 1, 1);
+		BasicDocument test7 = new BasicDocument("Sentence");
+
 		testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
+		BasicDocument test8 = new BasicDocument("Sentences?!");
+
 		testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
 		         32, 15, 1);
+		BasicDocument test9 = new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad.");
 		
 		
 	}
--- src/document/Document.java
+++ src/document/Document.java
@@ -53,7 +53,59 @@ public abstract class Document {
 		// TODO: Implement this method so that you can call it from the 
 	    // getNumSyllables method in BasicDocument (module 1) and 
 	    // EfficientDocument (module 2).
-	    return 0;
+
+		boolean isLoneE;
+		int vowelCount = 0;
+		String lowerCaseWord = word.toLowerCase();
+		String[] letterArray = lowerCaseWord.split("[^aeiouy]+");
+
+		// *** Quantifies number of vowel blocks present in the word ***
+		for(int i=0; i<letterArray.length; i++) {
+			if (letterArray[i].contains("a") || letterArray[i].contains("e") ||
+					letterArray[i].contains("i") || letterArray[i].contains("o") || letterArray[i].contains("u") ||
+					letterArray[i].contains("y")) {
+				vowelCount +=1;
+			}
+		}
+
+		// *** Evaluates whether the word ends with a single vowel block that ends with "e" ***
+		if(lowerCaseWord.endsWith("e") && vowelCount <= 1) {
+			isLoneE = true;
+		} else {
+			isLoneE = false;
+		}
+
+		// *** Returns number of value blocks alternatively if: (a) is a lone E; (b) ends with E, not a lone E,
+		// and preceded by a consonant, or (c) is not a lone E and doesn't end in a lone E
+		if( isLoneE ) {
+			return 1;
+		} else if( lowerCaseWord.endsWith("e") &&
+				(lowerCaseWord.charAt(lowerCaseWord.length() - 2) != 'a' &&
+				lowerCaseWord.charAt(lowerCaseWord.length() - 2) != 'e' &&
+				lowerCaseWord.charAt(lowerCaseWord.length() - 2) != 'i' &&
+				lowerCaseWord.charAt(lowerCaseWord.length() - 2) != 'o' &&
+				lowerCaseWord.charAt(lowerCaseWord.length() - 2) != 'u' &&
+				lowerCaseWord.charAt(lowerCaseWord.length() - 2) != 'y')) {
+			String[] vowelArray = word.split("[^aeiouy]+");
+			int syllCount = 0;
+			for(String vowel : vowelArray) {
+				if(vowel.contains("a") || vowel.contains("e") || vowel.contains("i") || vowel.contains("o") ||
+						vowel.contains("u") || vowel.contains("y")) {
+					syllCount += 1;
+				}
+			}
+			return (syllCount - 1);
+		} else {
+			String[] vowelArray = lowerCaseWord.split("[^aeiouy]+");
+			int syllCount = 0;
+			for(String vowel : vowelArray) {
+				if(vowel.contains("a") || vowel.contains("e") || vowel.contains("i") || vowel.contains("o") ||
+						vowel.contains("u") || vowel.contains("y")) {
+					syllCount += 1;
+				}
+			}
+			return syllCount;
+		}
 	}
 	
 	/** A method for testing
